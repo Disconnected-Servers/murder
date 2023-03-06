b2284fef41182b03d356c131758924247ef09db8
Marshall_vak <Marshall_vak@outlook.com>
2023-03-06 10:57:38 -0500

    Updatecl_ragdoll.lua | tehya-debug

--- gamemode/cl_ragdoll.lua
+++ gamemode/cl_ragdoll.lua
@@ -1,15 +1,11 @@
 local PLAYER = FindMetaTable("Player")
 local ENTITY = FindMetaTable("Entity")
 
-local IsValid = IsValid
-
-if not PLAYER.GetRagdollEntityOld then
-	PLAYER.GetRagdollEntityOld = PLAYER.GetRagdollEntity
-end
+PLAYER.GetRagdollEntityOld = PLAYER.GetRagdollEntityOld or PLAYER.GetRagdollEntity
 
 function PLAYER:GetRagdollEntity()
 	local ent = self:GetNWEntity("DeathRagdoll")
-	
+
 	if IsValid(ent) then
 		return ent
 	end
@@ -17,14 +13,14 @@ function PLAYER:GetRagdollEntity()
 	return self:GetRagdollEntityOld()
 end
 
-if not PLAYER.GetRagdollOwnerOld then
-	PLAYER.GetRagdollOwnerOld = PLAYER.GetRagdollOwner
-end
+ENTITY.GetRagdollOwnerOld = ENTITY.GetRagdollOwnerOld or ENTITY.GetRagdollOwner
 
 function ENTITY:GetRagdollOwner()
 	local ent = self:GetNWEntity("RagdollOwner")
+
 	if IsValid(ent) then
 		return ent
 	end
+
 	return self:GetRagdollOwnerOld()
 end
